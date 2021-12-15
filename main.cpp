226fab9fca4eb330aee2e4d9272faac5ee233189
Belikhun <belivipro9x99@gmail.com>
2021-12-15 11:23:16 +0700

    🧼 add counting total of created `SanPham`

--- main.cpp
+++ main.cpp
@@ -54,6 +54,28 @@ void printLuongOfCongNhan(CongNhanList cn, BangLuongList bl) {
 	}
 }
 
+void printTotalOfSanPham(SanPhamList::Node* root, BangLuongList bl) {
+	if (root == NULL)
+		return;
+
+	printTotalOfSanPham(root -> left, bl);
+
+	int tong = 0;
+	BangLuongList::Node* blNode;
+	for (blNode = bl.list.head; blNode != NULL; blNode = blNode -> next)
+		for (int i = 0; i < blNode -> info.soLuong; i++)
+			if (strcmp(blNode -> info.sanPham[i].maSP, root -> info.maSP) == 0)
+				tong += blNode -> info.sanPham[i].soLuong;
+		
+	cout << setw(10) << root -> info.maSP
+		 << setw(40) << truncate(root -> info.tenSP, 38)
+		 << setw(20) << tong
+		 << setw(20) << setprecision(1) << fixed << root -> info.donGia
+		 << endl;
+
+	printTotalOfSanPham(root -> right, bl);
+}
+
 int main() {
 	SetConsoleOutputCP(CP_UTF8);
 
@@ -72,10 +94,11 @@ int main() {
 	int cmd;
 	while (true) {
 		cout << endl;
-		cout << " 1) Quản lí danh sách công nhân" << endl;
-		cout << " 2) Quản lí danh sách sản phẩm" << endl;
-		cout << " 3) Quản lí bảng lương" << endl;
+		cout << " 1) Quản lí danh sách công nhân   (SINGLY LINKED LIST)" << endl;
+		cout << " 2) Quản lí danh sách sản phẩm    (TREE)" << endl;
+		cout << " 3) Quản lí bảng lương            (DOUBLY LINKED LIST)" << endl;
 		cout << " 4) Hiển thị tổng tiền lương của mỗi công nhân" << endl;
+		cout << " 5) Thống kê tổng số sản phẩm đã làm của mỗi sản phẩm" << endl;
 		cout << " 0) Thoát" << endl;
 
 		cout << endl << " > ";
@@ -99,6 +122,12 @@ int main() {
 			case 4:
 				printLuongOfCongNhan(congNhanList, bangLuongList);
 				break;
+
+			case 5: {
+				cout << "     Mã SP                                  Tên SP       Tổng Số Lượng             Đơn Giá" << endl;
+				printTotalOfSanPham(sanPhamList.tree, bangLuongList);
+				break;
+			}
 
 			case 0:
 			default:
